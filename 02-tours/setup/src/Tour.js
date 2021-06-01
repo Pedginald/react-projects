6e28c0713fc89f19cf1d1ecfd9179f003b02bc96
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-06-01 11:25:30 -0400

    Adding content to Tour component

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,8 +1,18 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
+const Tour = ({ id, image, info, name, price }) => {
 	return (
-		<h4>Hello, World!</h4>
+		<article className='single-tour'>
+			<img src={image} alt={name}/>
+			<footer>
+				<div className='tour-info'>
+					<h4>{name}</h4>
+					<h4 className='tour-price'>{price}</h4>
+				</div>
+				<p>{info}</p>
+				<button className='delete-btn'>not interested</button>
+			</footer>
+		</article>
 	)
 };
 
