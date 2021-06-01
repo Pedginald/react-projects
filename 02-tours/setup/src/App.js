b5abe7148ae8bce2127f9b8a49fd2220fb50ed1b
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-06-01 15:08:09 -0400

    Redoing compnents for practice

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -8,23 +8,30 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
-  const fetchTours = async () => {
-    setLoading(true);
+  const removeTour = (id) => {
+    const newTours = tours.filter(tour => tour.id !== id);
+    setTours(newTours);
+  }
 
+  const fetchTours = async () => {
+    setLoading(true)
+    
     try {
       const response = await fetch(url);
       const tours = await response.json();
       setLoading(false);
       setTours(tours);
+      console.log(tours);
     } catch (error) {
       setLoading(false);
       console.log(error);
     }
-  };
+  }
 
   useEffect(() => {
-    fetchTours();
-  }, []);
+    fetchTours()
+  }, [])
+
   if (loading) {
     return (
       <main>
@@ -32,9 +39,19 @@ function App() {
       </main>
     )
   }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={fetchTours}>show all tours</button>
+        </div>
+      </main>
+    )
+  }
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   )
 };
