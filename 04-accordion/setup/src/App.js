2efb5787cb46bb6ad37687939edf5ffb0bd00ed4
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-06-02 17:07:53 -0400

    Removing unused set state function from App file

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
 import data from './data';
-import Question from './Question';
 import SingleQuestion from './Question';
 
 function App() {
-  const [questions, setQuestions] = useState(data);
+  const [questions] = useState(data);
 
   return (
     <main>
@@ -12,7 +11,7 @@ function App() {
         <h3>questions and answers about login</h3>
         <section className='info'>
           {questions.map(question => {
-            return <Question key={question.id} {...question}/>
+            return <SingleQuestion key={question.id} {...question}/>
           })}
         </section>
       </div>
