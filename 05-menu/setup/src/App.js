3448b2f61e067eb5e1c0347f0d6865d6af865673
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-06-04 12:17:05 -0400

    Creating new set of unique category values

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,13 +3,16 @@ import Categories from './Categories';
 import Menu from './Menu';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map(item => item.category))];
+
 function App() {
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
   const [menuItems, setMenuItems] = useState(items);
 
   const filterItems = (category) => {
     if (category === 'all') {
       setMenuItems(items);
+      return;
     }
     const newItems = items.filter(item => item.category === category);
     setMenuItems(newItems);
@@ -22,7 +25,7 @@ function App() {
           <h2>our menu</h2>
           <div className='underline'></div>
         </div>
-          <Categories filterItems={filterItems}/>
+          <Categories filterItems={filterItems} categories={categories}/>
           <Menu items={menuItems}/>
       </section>
     </main>
